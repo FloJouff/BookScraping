ab3db661fd0915603d6a6e9a268e371f0d4e9827
FloJouff <141859687+FloJouff@users.noreply.github.com>
2024-01-04 15:05:43 +0100

    Amélioration de la note du livre

--- scrap.py
+++ scrap.py
@@ -5,7 +5,7 @@ import csv
 
 # adresse du livre à scraper:
 
-url = "https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
+url = "https://books.toscrape.com/catalogue/carrie_671/index.html"
 
 page = requests.get(url)
 
@@ -70,6 +70,15 @@ url_image = images[0].replace('../..', 'https://books.toscrape.com/')
 # Extraction de la notation
 
 note = soup.find("p", class_="star-rating")['class'][1]
+score_value = {
+    'One': '1',
+    'Two': '2',
+    'Three': '3',
+    'Four': '4',
+    'Five': '5'
+}
+
+note_finale = score_value[note]
 
 print("URL de la page produit: " + url)
 print("UPC:" + upc)
@@ -79,7 +88,7 @@ print("Nombre d'exemplaires disponibles: " + available_clean)
 print("Description du produit : " + product_description_clean)
 print("Category du livre : " + categorie)
 print("URL de l'image de couverture : " + url_image)
-print("La note du livre est : " + note)
+print("Note du livre : " + note_finale)
 
 # Préparation du fichier CSV
 
@@ -88,7 +97,7 @@ en_tete = ["URL", "Titre", "UPC", "Prix HT", "Prix TTC", "disponibilité",
            "Notation"]
 ligne1 = [url, title, upc, price_excluding_tax, price_including_tax,
           available_clean, categorie, product_description_clean, url_image,
-          note]
+          note_finale]
 
 # Création du fichier CSV
 
